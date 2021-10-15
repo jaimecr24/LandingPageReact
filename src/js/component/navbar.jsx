c47c698b1155e2adfe38b6e4f758fcbe442ee39f
Jaime <jaimecr72@gmail.com>
2021-10-15 13:56:08 +0000

    navbar done

---
+++ src/js/component/navbar.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Navbar = props => {
+	let itemsLi = props.otherItems.map((elem, index) => {
+		return (
+			<li key={index} className="nav-item">
+				<a className="nav-link" href="#">
+					{elem}
+				</a>
+			</li>
+		);
+	});
+	return (
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+			<div className="container-fluid">
+				<a className="navbar-brand" href="#">
+					{props.firstItem}
+				</a>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarNav"
+					aria-controls="navbarNav"
+					aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarNav">
+					<ul className="navbar-nav ms-auto">{itemsLi}</ul>
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+Navbar.propTypes = {
+	firstItem: PropTypes.string,
+	otherItems: PropTypes.array
+};
+
+export default Navbar;
--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,15 @@ import "../styles/index.scss";
 
 //import your own components
 import Home from "./component/home.jsx";
+import Navbar from "./component/navbar.jsx";
+
+const opcMenu = ["Home", "About", "Services", "Contact"];
+const opcFirst = "Start Bootstrap";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(
+	<div>
+		<Navbar firstItem={opcFirst} otherItems={opcMenu} />
+	</div>,
+	document.querySelector("#app")
+);
