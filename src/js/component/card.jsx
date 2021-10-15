594bcf424bdfc6effe40156f94cfe9fb4628abde
Jaime <jaimecr72@gmail.com>
2021-10-15 15:12:55 +0000

    cards done

---
+++ src/js/component/card.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Card = props => {
+	return (
+		<div className="card p-0 m-3 col">
+			<img src="https://fakeimg.pl/500x325/" className="card-img-top" />
+			<div className="card-body">
+				<h5 className="card-title">{props.title}</h5>
+				<p className="card-text">{props.text}</p>
+				<a href="#" className="btn btn-primary">
+					{props.textBtn}
+				</a>
+			</div>
+		</div>
+	);
+};
+
+Card.propTypes = {
+	title: PropTypes.string,
+	text: PropTypes.string,
+	textBtn: PropTypes.string
+};
+
+export default Card;
--- src/js/index.js
+++ src/js/index.js
@@ -12,11 +12,14 @@ import "../styles/index.scss";
 import Home from "./component/home.jsx";
 import Navbar from "./component/navbar.jsx";
 import Jumbotron from "./component/jumbotron.jsx";
+import Card from "./component/card.jsx";
 
 const opcMenu = ["Home", "About", "Services", "Contact"];
 const opcFirst = "Start Bootstrap";
 const textRnd =
 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+const textRndCard =
+	"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 
 //render your react application
 ReactDOM.render(
@@ -28,6 +31,28 @@ ReactDOM.render(
 				text={textRnd}
 				textBtn="Call to action!"
 			/>
+			<div className="row m-5">
+				<Card
+					title="Card title"
+					text={textRndCard}
+					textBtn="Find Out More!"
+				/>
+				<Card
+					title="Card title"
+					text={textRndCard}
+					textBtn="Find Out More!"
+				/>
+				<Card
+					title="Card title"
+					text={textRndCard}
+					textBtn="Find Out More!"
+				/>
+				<Card
+					title="Card title"
+					text={textRndCard}
+					textBtn="Find Out More!"
+				/>
+			</div>
 		</div>
 	</div>,
 	document.querySelector("#app")
